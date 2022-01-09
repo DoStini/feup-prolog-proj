e1eb8bceda31bdea5b8750e189b119c90edd1930
André Moreira <andre.moreira.9@hotmail.com>
2022-01-09 12:10:18 +0000

    Add color

--- game.pl
+++ game.pl
@@ -1,14 +1,20 @@
 :- use_module(library(lists)).
 
+opposite(red,  blue).
+opposite(blue, red).
+
+color(1, blue).
+color(2, red).
+
 generateLine(Size, Start, NewLine) :- generateLine(Size, Start, NewLine, 0, []).
 
 generateLine(Size, Start, NewLine, Size, NewLine) :- !.
 generateLine(Size, Start, NewLine, Acc, Prev) :-
     Acc < Size,
-    Color is mod(mod(Acc, 2) + Start, 2),
-    format('~*t', [Color]),
+    ColorIdx is mod(mod(Acc, 2) + Start, 2) + 1,
+    % ColorIdx is mod(Acc, 2) + Start + 1,
+    color(ColorIdx, Color),
     Elem = [Color],
-
     append(Prev, Elem, Combined),
     NextAcc is Acc + 1,
     generateLine(Size, Start, NewLine, NextAcc, Combined).
@@ -27,22 +33,35 @@ generateBoard(Size, Board, Acc, Prev) :-
     NextAcc is Acc + 1,
     generateBoard(Size, Board, NextAcc, Combined).
 
-boardCenter(Board, Res) :-
+boardCenter(Board, ResX/ResY) :-
     length(Board, C),
     X is (C - 1) / 2,
-    Res = [X, X].
+    ResX is X,
+    ResY is X.
 
-distSqr(P1, P2, Res) :-
-    nth0(0, P1, P1x),
-    nth0(1, P1, P1y),
-    nth0(0, P2, P2x),
-    nth0(1, P2, P2y),
+distSqr(P1x/P1y, P2x/P2y, Res) :-
     Res is (P2y - P1y) ** 2 + (P2x - P1x) ** 2.
 
-distInc(Board, CurrentPos, TargetPos) :- 
-    boardCenter(Board, Center),
-    distSqr(CurrentPos, Center, Current),
-    distSqr(TargetPos, Center, Target),
-    nth0(0, Center, X),
-    format('~2f ~2f ~2f', [Current, Target, X]),
+distInc(Board, CurrX/CurrY, Tx/Ty) :- 
+    boardCenter(Board, Cx/Cy),
+    distSqr(CurrX/CurrY, Cx/Cy, Current),
+    distSqr(Tx/Ty, Cx/Cy, Target),
     Target > Current.
+
+verifyPlayerCell(Board, Player, X/Y) :-
+    nth0(Y, Board, Line),
+    nth0(X, Line, Cell),
+    Cell is Player.
+
+test(Size):-
+    generateBoard(Size, Board),
+    verifyPlayerCell(Board, 1, 2/2).
+
+
+test2(Size):-
+    generateBoard(Size, Board),
+    verifyPlayerCell(Board, 1, 2/2).
+
+
+% move(Board, Px/Py, Dir/Capt) :-
+    
