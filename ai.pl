05f2d7c4530c03a56408411edd2071bbf366c2af
André Moreira <andre.moreira.9@hotmail.com>
2022-01-19 14:53:45 +0000

    Add ai menu selection

--- ai.pl
+++ ai.pl
@@ -1,23 +1,30 @@
 :- use_module(library(random)).
+:- [input].
 
-% choose_move(GameState, human, Move):-
+choose_move(Board/_, human, Move) :-
+    length(Board, Size),
+    askMove(Size, Move).
 
-% move(Board/Player, Px/Py/Dir/Conquer, NewBoard/Next) :-
-choose_move(Board, computer-Level, Move):-
-    valid_moves(Board, Moves),
-    choose_move(Level, Board, Moves, Move).
+choose_move(GameState, Type, Move):-
+    write(Type),
+    valid_moves(GameState, Moves),
+    choose_move(Type, GameState, Moves, Move).
 
-choose_move(1, _Board, Moves, Move):-
+choose_move(random, _GameState, Moves, Move):-
     random_member(Move, Moves).
 
-choose_move(2, Board, Moves, Move):-
+choose_move(smart, Board/Player, Moves, Move):-
     setof(Value-Mv, NewBoard^( 
         member(Mv, Moves),
         move(Board/Player, Move, NewBoard/_),
         value(NewBoard, Player, Value) 
-    ), [_V-Move|_]).
+    ), [_V-Move|_]),
+    write('hello: '),
+    write(Move),
+    nl.
+
 
 test_choose(Move) :-
     generateBoard(5, Board),
     valid_moves(Board/red, Moves),
-    choose_move(1, Board, Moves, Move).
+    choose_move(human, Board, Moves, Move).
--- main.pl
+++ main.pl
@@ -1,76 +1,23 @@
 :- [game].
 :- [display].
 :- [ai].
+:- [input].
 
-player('red', red).
-player('blue', blue).
-
-dir('up', up).
-dir('up_right', up_right).
-dir('right', right).
-dir('down_right', down_right).
-dir('down', down).
-dir('down_left',down_left).
-dir('left', left).
-dir('up_left', up_left).
-
-conquer('y', true).
-conquer('n', false).
-
-match(X, X).
-
-askNumber(Text, Min, Max, Number) :-
-    repeat,
-    write(Text),
-    read(X),
-    number(X),
-    X >= Min,
-    X < Max,
-    Number = X, !.
-
-askPlayer(Player) :-
-    repeat,
-    write('Please input initial player: (red/blue) '),
-    read(X),
-    player(X, Player), !.
-
-askConfig(Size, FirstPlayer) :-
-    askNumber('Please input Size: ', 1, 1000000, Size),
-    askPlayer(FirstPlayer).
-
-askDir(Dir) :-
-    repeat,
-    write('Please input direction: '),
-    read(X),
-    dir(X, Dir), !.
-
-askConquer(Conquer) :-
-    repeat,
-    write('Conquer? (Y/N) '),
-    read(X),
-    conquer(X, Conquer), !.
-
-askMove(Size, X, Y, Dir, Conquer) :-
-    askNumber('Please input X: ', 0, Size, X),
-    askNumber('Please input Y: ', 0, Size, Y),
-    askDir(Dir),
-    askConquer(Conquer).
-
-gameCycle(Board/_, _Size) :-
-    end_game(Board, PlayerYes), !,
+gameCycle(Board/_/_/_, _Size) :-
+    end_game(Board, Player), !,
     drawGame(Board),
-    format("~s~a~s", ["\nWinner is ", PlayerYes, "!\n\n"]).
+    format("~s~a~s", ["\nWinner is ", Player, "!\n\n"]).
 
-gameCycle(Board/Player, Size) :-
+gameCycle(GameState/Player1/Player2, Size) :-
     (
         repeat,
-        display_game(Board/Player),
-        askMove(Size, X, Y, Dir, Conquer),
-        (move(Board/Player, X/Y/Dir/Conquer, NewBoard/Next) ; (format("~s", ["!!INVALID MOVE, TRY AGAIN!!\n"]), fail))
+        display_game(GameState),
+        choose_move(GameState, Player1, Move),
+        (move(GameState, Move, NextState) ; (format("~s", ["!!INVALID MOVE, TRY AGAIN!!\n"]), fail))
     ),
-    gameCycle(NewBoard/Next, Size).
+    gameCycle(NextState/Player2/Player1, Size).
 
 play :-
-    askConfig(Size, FirstPlayer),
+    askConfig(Size, FirstPlayer, Player1/Player2),
     generateBoard(Size, Board),
-    gameCycle(Board/FirstPlayer, Size).
+    gameCycle(Board/FirstPlayer/Player1/Player2, Size).
